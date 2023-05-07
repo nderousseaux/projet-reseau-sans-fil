3bda277a90610850ba07178e141e7dff8cf6bc24
vandrianandrasan <vagnona.andrianandrasana-dina@etu.unistra.fr>
2023-05-07 18:59:17 +0200

    rm

--- Scénarios/Scénario 0/tsch-orchestra/build/iotlab/m3/obj/crc16.d
+++
@@ -1,4 +0,0 @@
-build/iotlab/m3/obj/crc16.o: ../../os/lib/crc16.c \
- ../../os/services/shell/module-macros.h
-../../os/lib/crc16.c :
- ../../os/services/shell/module-macros.h :
--- Scénarios/script_scp.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-scp -r wifi2023stras4@strasbourg.iot-lab.info:~/iot-lab/parts/iot-lab-contiki-ng/contiki-ng/examples/tsch-orchestra .
