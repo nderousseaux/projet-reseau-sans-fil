38922cda42371537fe0f8581cb50998f77a58cf9
vandrianandrasan <vagnona.andrianandrasana-dina@etu.unistra.fr>
2023-05-07 18:59:17 +0200

    rm

--- Scénarios/Scénario 0/tsch-orchestra/build/iotlab/m3/1er_script.sh
+++
@@ -1,7 +0,0 @@
-# Lancer un scénarios entre 2 noeuds où l'on mesure le traffic
-
-#!/bin/bash
-
-iotlab-experiment submit -d 10 -l 1,archi=m3:at86rf231+site=strasbourg,coordinator.iotlab,profile1 -l 1,archi=m3:at86rf231+site=strasbourg,sender.iotlab,profile1
-
-
--- Scénarios/Scénario 0/tsch-orchestra/build/iotlab/m3/obj/nullradio.d
+++
@@ -1,6 +0,0 @@
-build/iotlab/m3/obj/nullradio.o: ../../os/dev/nullradio.c \
- ../../os/services/shell/module-macros.h ../../os/dev/nullradio.h \
- ../../os/dev/radio.h
-../../os/dev/nullradio.c :
- ../../os/services/shell/module-macros.h ../../os/dev/nullradio.h :
- ../../os/dev/radio.h :
