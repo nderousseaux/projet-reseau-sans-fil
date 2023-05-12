da942479bcf534c8eeb7050df8131c4333594acc
Derousseaux Nathanaël <n.derousseaux@icloud.com>
2023-05-12 19:41:44 +0200

    feur

--- Plots/main.py
+++ Plots/main.py
@@ -27,10 +27,10 @@ def analyse_nb_senders(path):
 	# plt.ylabel("Consommation moyenne des senders (W)")
 
 	# On affiche le graphique
-	plt.show()
+	# plt.show()
 
 	# On fait un histogramme du débit utile du réseau
-	plt.bar([str(s.nbSenders) for s in scenarios], [s.get_useful_throughput() for s in scenarios])
+	plt.bar([str(s.nbSenders) for s in scenarios], [s.get_mean_useful_throughput_senders() for s in scenarios])
 
 	# Titre
 	plt.title("Débit utile du réseau en fonction du nombre de senders")
@@ -41,6 +41,20 @@ def analyse_nb_senders(path):
 
 	# On affiche le graphique
 	plt.show()
+
+	# On fait un histogramme du taux de perte du réseau
+	plt.bar([str(s.nbSenders) for s in scenarios], [s.get_mean_loss_rate() for s in scenarios])
+
+	# Titre
+	plt.title("Taux de perte du réseau en fonction du nombre de senders")
+
+	# On affiche les labels
+	plt.xlabel("Nombre de senders")
+	plt.ylabel("Taux de perte du réseau (%)")
+
+	# On affiche le graphique
+	plt.show()
+
 
 	
 def analyse_size_slot(path):
@@ -60,24 +74,54 @@ def analyse_size_slot(path):
 	# Titre
 	plt.title("Consommation moyenne des senders en fonction de la taille des slots")
 
-	# On affiche les labels
-	plt.xlabel("Taille des slots")
+	# On affiche les labels en microsecondes
+	plt.xlabel("Taille des slots en microsecondes")
 	plt.ylabel("Consommation moyenne des senders (W)")
 
 	# On affiche le graphique
 	plt.show()
 
+	# On fait un histogramme du débit utile du réseau
+	plt.bar([str(s.slotSize) for s in scenarios], [s.get_mean_useful_throughput_senders() for s in scenarios])
+
+	# Titre
+	plt.title("Débit utile du réseau en fonction de la taille des slots")
+
+	# On affiche les labels
+	plt.xlabel("Taille des slots en microsecondes")
+	plt.ylabel("Débit utile du réseau (kbps)")
+
+	# On affiche le graphique
+	plt.show()
+
+	# On fait un histogramme du taux de perte du réseau
+	plt.bar([str(s.slotSize) for s in scenarios], [s.get_mean_loss_rate() for s in scenarios])
+
+	# Titre
+	plt.title("Taux de perte du réseau en fonction de la taille des slots")
+
+	# On affiche les labels
+	plt.xlabel("Taille des slots en microsecondes")
+	plt.ylabel("Taux de perte du réseau")
+
+	# On affiche le graphique
+	plt.show()
+
+
 
 if __name__ == "__main__":	
+	path = "Scenarios"
+
 	# On écrit en dur le scénario étalon
-	# scenario_etalon = Scenario(path + "/s0-etalon", 5, TSCH, 397, 600)
+	scenario_etalon = Scenario(path + "/s0-etalon", 5, TSCH, 397, 600)
 
 	# # On analyse le nombre de senders
-	# analyse_nb_senders(path)
+	analyse_nb_senders(path)
+	# analyse_size_slot(path)
 
-	# Exemple
-	s = Scenario("Scenario/s1-nb-senders/2", 2, TSCH, 397, 600)
+	# # Exemple
+	# s = Scenario("Scenario/s1-nb-senders/2", 2, TSCH, 397, 600)
 
-	print(s)
+	# print(s)
 
 
