ee467a3c8bf6072f3d1d5fea55f3d2a8d9a172ba
Nathanaël Derousseaux <n.derousseaux@icloud.com>
2023-05-12 11:35:31 +0200

    Avancée consumption

--- Plots/main.py
+++ Plots/main.py
@@ -1,15 +1,17 @@
 import sys
 from Scenario import *
 import matplotlib.pyplot as plt
-if __name__ == "__main__":	
-	path = sys.argv[1]
+
+scenario_etalon = None
+
+def analyse_nb_senders(path):
 
 	# On écrit en dur les scénarios
 	scenarios = [
-		Scenario(path + "/scenario1-nb-senders/1", 1, TSCH, 397, 600),
-		Scenario(path + "/scenario1-nb-senders/2", 2, TSCH, 397, 600),
-		Scenario(path + "/scenario0-etalon", 5, TSCH, 397, 600),
-		Scenario(path + "/scenario1-nb-senders/10", 10, TSCH, 397, 600),
+		Scenario(path + "/s1-nb-senders/1", 1, TSCH, 397, 600),
+		Scenario(path + "/s1-nb-senders/2", 2, TSCH, 397, 600),
+		scenario_etalon,
+		Scenario(path + "/s1-nb-senders/10", 10, TSCH, 397, 600),
 	]
 	
 	# On fait un histogramme de la consommation moyenne des senders get_power_mean_senders()
@@ -26,3 +28,36 @@ if __name__ == "__main__":
 
 	# On affiche le graphique
 	plt.show()
+	
+def analyse_size_slot(path):
+		# On écrit en dur les scénarios
+	scenarios = [
+		Scenario(path + "/s3-size-slot/100", 5, TSCH, 100, 600),
+		Scenario(path + "/s3-size-slot/200", 5, TSCH, 200, 600),
+		scenario_etalon,
+		Scenario(path + "/s3-size-slot/1000", 5, TSCH, 1000, 600),
+	]
+	
+	# On fait un histogramme de la consommation moyenne des senders get_power_mean_senders()
+	plt.bar([str(s.slotSize) for s in scenarios], [s.get_power_mean_senders() for s in scenarios])
+
+	# Echelle logarithmique
+	plt.yscale("log")
+	# Titre
+	plt.title("Consommation moyenne des senders en fonction de la taille des slots")
+
+	# On affiche les labels
+	plt.xlabel("Taille des slots")
+	plt.ylabel("Consommation moyenne des senders (W)")
+
+	# On affiche le graphique
+	plt.show()
+
+
+if __name__ == "__main__":	
+	path = sys.argv[1]
+
+	scenario_etalon = Scenario(path + "/s0-etalon", 5, TSCH, 397, 600)
+
+	analyse_size_slot(path)
+
